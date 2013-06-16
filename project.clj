17dedfbacaa071327615848fbd4f6c5abb6707af
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2013-06-16 11:22:22 +0200

    set project version to 0.1.0 for release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject us.bpsm/sft "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject us.bpsm/sft "0.1.0"
+  :description "Stupid-Fast Template"
+  :url "https://github.com/bpsm/stupid-fast-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
